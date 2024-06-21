1a835148dd6fb285bc9e960dd86ba6589b7af828
Diego Villa <dvillanueva@sdi.es>
2024-06-21 20:09:44 +0200

    feat: Implement i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'language-root',
+  selector: "language-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, TranslateModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-  title = 'angular-internationalization';
+  title: string = "";
+
+  constructor(private _translate: TranslateService) {
+    this._translate.setDefaultLang("en");
+  }
+
+  switchLanguage(language: string) {
+    this._translate.use(language);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HttpClient, provideHttpClient } from "@angular/common/http";
+import { importProvidersFrom } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
+import { HttpLoaderFactory } from "./i18n";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
+  ],
+  ...appConfig.providers
+}).catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+});
